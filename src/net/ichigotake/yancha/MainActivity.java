4ade2bfd603ad5635a7dea0babaf1ad7b63a0150
Takayuki Otake <k.wisiiy@gmail.com>
2013-08-11 00:18:39 +0900

    過去ログ検索はアクションバーから遷移するようにする
    TODO: 過去ログ検索の実装

--- src/net/ichigotake/yancha/MainActivity.java
+++ src/net/ichigotake/yancha/MainActivity.java
@@ -1,29 +1,25 @@
 package net.ichigotake.yancha;
 
+import net.ichigotake.yancha.core.actionbar.ActionBarSearchable;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
-
-	Fragment fragment;
-	
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.yc_main);
-		
-		FragmentManager manager = getSupportFragmentManager();
-		fragment = manager.findFragmentById(R.id.wrap_fragment);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		
+		new ActionBarSearchable(this).setup(menu);
+		
+		return super.onCreateOptionsMenu(menu);
 	}
-
+	
 }
---
+++ src/net/ichigotake/yancha/core/actionbar/ActionBarSearchable.java
@@ -0,0 +1,70 @@
+package net.ichigotake.yancha.core.actionbar;
+
+import net.ichigotake.yancha.R;
+import net.ichigotake.yancha.search.ChatLogSearchFragment;
+import net.ichigotake.yancha.ui.FragmentTransit;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+
+/**
+ * アクションバーに検索窓を設置
+ */
+public class ActionBarSearchable {
+
+	final private FragmentManager fragmentManager;
+	
+	final private FragmentActivity fragmentActivity;
+	
+	public ActionBarSearchable(FragmentActivity fragmentActivity) {
+		this.fragmentActivity = fragmentActivity;
+		fragmentManager = fragmentActivity.getSupportFragmentManager();
+	}
+	
+	public void setup(Menu menu) {
+		SearchManager searchManager = (SearchManager) fragmentActivity.getSystemService(Context.SEARCH_SERVICE);
+		SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+		SearchableInfo searchable = searchManager.getSearchableInfo(fragmentActivity.getComponentName());
+		searchView.setSearchableInfo(searchable);
+		searchView.setIconifiedByDefault(false);
+		searchView.setOnQueryTextListener(new SearchableOnQueryTextListener());		
+	}
+	
+	private class SearchableOnQueryTextListener implements OnQueryTextListener {
+			
+		@Override
+		public boolean onQueryTextSubmit(String query) {
+			if ("".equals(query)) {
+				return false;
+			}
+			
+			doSearch(query);
+			return true;
+		}
+		
+		@Override
+		public boolean onQueryTextChange(String arg0) {
+			return false;
+		}
+
+		private void doSearch(String query) {
+			final Bundle args = new Bundle();
+			args.putString(SearchManager.QUERY, query);
+			
+			Fragment nextFragment = ChatLogSearchFragment.createInstance();
+			nextFragment.setArguments(args);
+			
+			Fragment currentFragment = fragmentManager.findFragmentById(R.id.wrap_fragment);
+			new FragmentTransit(currentFragment).toNext(nextFragment);
+		}
+
+	}
+
+}
