19337bac0b4bc5be2f76152c32f95ad0f877edcf
ichigotake <k.wisiiy@gmail.com>
2014-02-23 14:36:02 +0900

    デバッグ用にcrashlyticsをセット

--- Yancha/src/main/java/net/ichigotake/yancha/common/context/BaseApplication.java
+++ Yancha/src/main/java/net/ichigotake/yancha/common/context/BaseApplication.java
@@ -2,11 +2,18 @@ package net.ichigotake.yancha.common.context;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
+import net.ichigotake.yancha.BuildConfig;
+
 public class BaseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Crashlytics.start(this);
+        }
         System.setProperty("http.agent", new AppContext(this).getUserAgent());
     }
 }
