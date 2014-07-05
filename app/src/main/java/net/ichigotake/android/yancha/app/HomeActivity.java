53c188b917d87dd92b1155e87dea8860696ee066
ichigotake <k.wisiiy@gmail.com>
2014-07-05 12:23:08 +0900

    新しい開発スタイル

---
+++ app/src/main/java/net/ichigotake/android/yancha/app/HomeActivity.java
@@ -0,0 +1,14 @@
+package net.ichigotake.android.yancha.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
