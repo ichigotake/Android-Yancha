c2d5ae8468af7fb1ac9460165ca3c8355ea78ff1
ichigotake <ichigotake.san@gmail.com>
2014-07-20 07:20:27 +0900

    発言を投稿する

--- app/src/main/java/net/ichigotake/android/yancha/app/chat/ChatMessageInputFragment.java
+++ app/src/main/java/net/ichigotake/android/yancha/app/chat/ChatMessageInputFragment.java
@@ -2,17 +2,48 @@ package net.ichigotake.android.yancha.app.chat;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import net.ichigotake.android.common.os.BundleMerger;
 import net.ichigotake.android.yancha.app.R;
 
 public class ChatMessageInputFragment extends Fragment {
 
+    private final String KEY_INPUT_TEXT = "input_text";
+    private EditText inputTextArea;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat_messages_input, parent, false);
+        View view = inflater.inflate(R.layout.fragment_chat_messages_input, parent, false);
+        inputTextArea = (EditText)view.findViewById(R.id.fragment_chat_messages_input_text_area);
+        inputTextArea.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String text = v.getText().toString().trim();
+                if (EditorInfo.IME_ACTION_SEND == actionId && !TextUtils.isEmpty(text)) {
+                    ((SocketIoClientActivity)getActivity()).getSocketIoClient()
+                            .emit(SocketIoEvent.USER_MESSAGE, text);
+                    v.setText("");
+                }
+                return false;
+            }
+        });
+        inputTextArea.setText(BundleMerger.merge(savedInstanceState).getString(KEY_INPUT_TEXT, ""));
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY_INPUT_TEXT,
+                inputTextArea != null ? inputTextArea.getText().toString() : "");
     }
 
 }
