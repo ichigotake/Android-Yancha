0af65a9bde7a321a2825a31048e30d4dc0679a54
ichigotake <ichigotake.san@gmail.com>
2014-07-20 05:46:33 +0900

    Activityを開いた際にキーボードを開かない

--- app/src/main/java/net/ichigotake/android/yancha/app/terminal/phone/ChatActivity.java
+++ app/src/main/java/net/ichigotake/android/yancha/app/terminal/phone/ChatActivity.java
@@ -3,6 +3,7 @@ package net.ichigotake.android.yancha.app.terminal.phone;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 
 import net.ichigotake.android.yancha.app.chat.ChatFragment;
 import net.ichigotake.android.yancha.app.R;
@@ -11,6 +12,7 @@ public final class ChatActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
